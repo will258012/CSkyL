53d8197fc0e1e4b1223edda63d909309b61d7cc8
will258012 <will258012@gmail.com>
2024-04-20 07:42:55 +0800

    Update Utils.cs
    - 移动 `AssemblyPath` 属性
    - 将 `GetConfigLanguageIndex()` 改为属性 `ConfigLanguageIndex`

--- Translation/Utils.cs
+++ Translation/Utils.cs
@@ -1,9 +1,42 @@
-﻿using System.Xml;
+﻿using ColossalFramework.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml;
+using static ColossalFramework.Plugins.PluginManager;
 
 namespace CSkyL.Translation
 {
     internal class Utils
     {
+        internal static string AssemblyPath {
+            get {
+
+                // No path cached - get list of currently active plugins.
+                Assembly thisAssembly = Assembly.GetExecutingAssembly();
+                IEnumerable<PluginInfo> plugins = PluginManager.instance.GetPluginsInfo();
+
+                // Iterate through list.
+                foreach (PluginInfo plugin in plugins) {
+                    try {
+                        // Iterate through each assembly in plugins
+                        foreach (Assembly assembly in plugin.GetAssemblies()) {
+                            if (assembly == thisAssembly) {
+                                return plugin.modPath;
+                            }
+                        }
+                    }
+                    catch (Exception e) {
+                        // Don't care; just don't let a single failure stop us iterating through the plugins.
+                        Log.Err("[Translator] " + e + "exception iterating through plugins");
+                    }
+                }
+
+                // If we got here, then we didn't find the assembly.
+                Log.Err("[Translator] assembly path not found");
+                return null;
+            }
+        }
         /// <summary>
         /// Read the "Language" key value pairs in the mod configuration file.
         /// In this way, when using `Translates.Translate` method for the first time, the language can be loaded correctly.
@@ -12,23 +45,23 @@ namespace CSkyL.Translation
         /// Returns the language index from the configuration file. 
         /// Returns 0 if the "Language" node is not found.
         /// </returns>
-        internal static int GetConfigLanguageIndex()
-        {
-            string filePath = "FPSCameraConfig.xml";
+        internal static int ConfigLanguageIndex {
+            get {
+                string filePath = "FPSCameraConfig.xml";
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load(filePath);
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
 
-            XmlNode languageNode = doc.SelectSingleNode("/Config/Language");
+                XmlNode languageNode = doc.SelectSingleNode("/Config/Language");
 
-            if (languageNode != null) {
-                return int.Parse(languageNode.InnerText);
+                if (languageNode != null) {
+                    return int.Parse(languageNode.InnerText);
+                }
+                else {
+                    return 0;
+                }
+
             }
-            else {
-                return 0;
-            }
-
         }
-
     }
 }
