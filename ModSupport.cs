fd59aa98de9ac6b28f6f81ed72966800c7f9ba28
will258012 <will258012@gmail.com>
2024-04-26 22:29:34 +0800

    移动 `ModSupport` 类
    - 同时修复 `FindToggleIt` 未按预期工作的问题

---
+++ ModSupport.cs
@@ -0,0 +1,28 @@
+﻿using ColossalFramework.Plugins;
+namespace CSkyL.ModSupport
+{
+    class ToggleIt
+    {
+        public static bool IsToggleItFoundandEnbled { get; private set; }
+        public static void FindToggleIt()
+        {
+            try {
+                var infos = PluginManager.instance.GetPluginsInfo();
+                if (PluginManager.instance.isActiveAndEnabled) {
+                    foreach (var info in infos) {
+                        if ((info.publishedFileID.AsUInt64 == 1764637396 || info.publishedFileID.AsUInt64 == 2573796841) && info.isEnabled) {
+                            Log.Msg("[ToggleItSupport] \"Toggle It!\" (or its CHS version) was found!");
+                            IsToggleItFoundandEnbled = true;
+                            return;
+                        }
+                    }
+                    IsToggleItFoundandEnbled = false;
+                }
+            }
+            catch (System.Exception e) {
+                Log.Err($"[ToggleItSupport] Something went wrong when finding the mod: {e}");
+                IsToggleItFoundandEnbled = false;
+            }
+        }
+    }
+}
