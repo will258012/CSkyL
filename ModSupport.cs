e22b477cd0f20aa61d0aa11368c3bb1a3176b76f
will258012 <125041335+will258012@users.noreply.github.com>
2024-05-01 16:34:40 +0800

    优化检测模组方法

--- ModSupport.cs
+++ ModSupport.cs
@@ -1,6 +1,4 @@
 ﻿using ColossalFramework.Plugins;
-using System.Collections.Generic;
-using static ColossalFramework.Plugins.PluginManager;
 namespace CSkyL
 {
     public class ModSupport
@@ -10,51 +8,24 @@ namespace CSkyL
         public static ushort FollowVehicleID { get; set; }
         internal static void Initialize()
         {
-            ModChecker.CheckToggleIt();
-            ModChecker.CheckTrainDisplay();
-        }
-        private class ModChecker
-        {
-            internal static void CheckToggleIt()
-            {
-                try {
-                    if (PluginManager.instance.isActiveAndEnabled) {
-                        foreach (var info in infos) {
-                            if ((info.publishedFileID.AsUInt64 == 1764637396 || info.publishedFileID.AsUInt64 == 2573796841) && info.isEnabled) {
-                                Log.Msg("[ToggleItSupport] \"Toggle It!\" (or its CHS version) was found!");
-                                IsToggleItFoundandEnbled = true;
-                                return;
-                            }
-                        }
-                        IsToggleItFoundandEnbled = false;
+            try {
+                var infos = PluginManager.instance.GetPluginsInfo();
+                foreach (var info in infos) {
+                    if ((info.publishedFileID.AsUInt64 == 1764637396 || info.publishedFileID.AsUInt64 == 2573796841) && info.isEnabled) {
+                        Log.Msg("[ModSupport] \"Toggle It!\" (or its CHS version) was found!");
+                        IsToggleItFoundandEnbled = true;
+                        continue;
+                    }
+                    if ((info.publishedFileID.AsUInt64 == 3233229958 || info.name.Contains("TrainDisplay")) && info.isEnabled) {
+                        Log.Msg("[ModSupport] \"Train Display - Update\" was found!");
+                        IsTrainDisplayFoundandEnbled = true;
+                        continue;
                     }
                 }
-                catch (System.Exception e) {
-                    Log.Err($"[ToggleItSupport] Something went wrong when finding the mod: {e}");
-                    IsToggleItFoundandEnbled = false;
-                }
             }
-            internal static void CheckTrainDisplay()
-            {
-                try {
-                    if (PluginManager.instance.isActiveAndEnabled) {
-                        foreach (var info in infos) {
-                            if (info.publishedFileID.AsUInt64 == 3233229958 && info.isEnabled) {
-                                Log.Msg("[TrainDisplayUpdateSupport] \"Train Display - Update\" was found!");
-                                IsTrainDisplayFoundandEnbled = true;
-                                FollowVehicleID = default;
-                                return;
-                            }
-                        }
-                        IsTrainDisplayFoundandEnbled = false;
-                    }
-                }
-                catch (System.Exception e) {
-                    Log.Err($"[TrainDisplayUpdateSupport] Something went wrong when finding the mod: {e}");
-                    IsTrainDisplayFoundandEnbled = false;
-                }
+            catch (System.Exception e) {
+                Log.Err($"[ModSupport] Something went wrong when finding the mod: {e}");
             }
-            private static IEnumerable<PluginInfo> infos => PluginManager.instance.GetPluginsInfo();
         }
     }
 }
