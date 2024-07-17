51777cd59d3fd37c5f656b6b9d32851dba8221f5
will258012 <125041335+will258012@users.noreply.github.com>
2024-07-17 22:11:12 +0800

    添加“阴影距离优化”功能

---
+++ Game/Control/ShadowsManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace CSkyL.Game.Control
+{
+    public class ShadowsManager
+    {
+        public static IEnumerator ToggleShadowsOptimization(bool status)
+        {
+            try {
+                Log.Msg("-- Setting shadows distance");
+                if (status) {
+                    beforeOptimization = UnityEngine.QualitySettings.shadowDistance;
+                    UnityEngine.QualitySettings.shadowDistance = Optimization;
+                }
+                else {
+                    UnityEngine.QualitySettings.shadowDistance = beforeOptimization;
+                }
+            }
+
+            catch (Exception e) {
+                Log.Err("Unrecognized Error:" + e);
+            }
+
+            yield break;
+        }
+        private static float beforeOptimization;
+        private const float Optimization = 512f;
+    }
+
+}
